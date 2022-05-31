b01f14ca0edfcb6ffd00f406a8ff8cd77b86a1e9
aaa <aaa>
2022-05-31 17:02:59 +0900

    結果にコミットする男

--- linetrase.X/main.c
+++ linetrase.X/main.c
@@ -9,7 +9,7 @@
 #define LED_LEFT_2 1
 #define LED_RIGHT_1 2
 #define LED_RIGHT_2 3
-// LED_LEFT_1, LED_LEFT_2, LED_RIGHT_1, LED_RIGHT_2 の順で配置
+
 #define MOTOR_LEFT 0
 #define MOTOR_RIGHT 1
 #define CRANK_POSSIBLY 3
@@ -19,113 +19,60 @@
 
 #define LINE_CROSSED(x, i) (x[i] < LED_THRESHOLD)
 #define SUB(x) ((x == 0) ? (0) : (x - 1))
-#define MAP(x) ((x > INT8_MAX) ? (INT8_MAX) : ((x < INT8_MIN) ? (INT8_MIN) : (x)))
-
-void ReadLedBar(uint8_t x[LED_NUM]);
-void SetMotorValue(const uint8_t x[LED_NUM], int8_t y[MOTOR_NUM]);
-void MotorOut(const int8_t y[MOTOR_NUM]);
-// void TMR0_Interrupt(void);
-void MOTOR_WRITE_PWM(const int8_t y[MOTOR_NUM], const uint8_t i, const uint8_t port);
+#define MAP(x) ((x > (INT8_MAX)) ? (INT8_MAX) : ((x < (INT8_MIN)) ? (INT8_MIN) : (x)))
 
 void main(void)
 {
-	SYSTEM_Initialize();
-	INTERRUPT_PeripheralInterruptEnable();
-	// TMR0_SetInterruptHandler(TMR0_Interrupt);
-	INTERRUPT_GlobalInterruptEnable();
-	uint8_t ledBar[LED_NUM] = {0};
-	int16_t motorOrder[MOTOR_NUM] = {64, 64};
-	//-128 ~ -17 で後退,-16 ~ 15でブレーキ(0は空転), 16 ~ 127で前進
-	while (1)
-	{
-		if (flag == 1)
-			ReadLedBar(ledBar);
-		SetMotorValue(ledBar, motorOrder);
-		MotorOut(motorOrder);
-	}
-}
-void ReadLedBar(uint8_t x[LED_NUM])
-{
-	for (uint8_t i = channel_AN0; i <= channel_AN3; i++)
-		x[i] = ADC_GetConversion(i) >> 8;
-	return;
-}
-void SetMotorValue(const uint8_t x[LED_NUM], int16_t y[MOTOR_NUM])
-{
-	bool left_line_crossed = LINE_CROSSED(x, LED_LEFT_2), right_line_crossed = LINE_CROSSED(x, LED_RIGHT_1), right_double_line = LINE_CROSSED(x, LED_RIGHT_1) && LINE_CROSSED(x, LED_RIGHT_2), left_double_line = LINE_CROSSED(x, LED_LEFT_1) && LINE_CROSSED(x, LED_LEFT_2);
-	static bool crank_coming = false, lane_change_coming = false;
-	static uint8_t crank_possibly = 0, lane_change_possibly = 0, crank_postponement = 0, lane_change_postponement = 0;
-	crank_possibly = SUB(crank_possibly);
-	lane_change_possibly = SUB(lane_change_possibly);
-	if (left_double_line)
-	{
-		if (right_double_line)
-			if (lane_change_possibly < LANE_CHANGE_POSSIBLY)
-				lane_change_possibly += LANE_CHANGE_POSSIBLY;
-			else
-				lane_change_coming = true;
-		else
-		{
-			if (crank_possibly < CRANK_POSSIBLY)
-				crank_possibly += CRANK_POSSIBLY;
-			else
-				crank_coming = true;
-		}
-	}
-	if (lane_change_coming)
-		lane_change_postponement++;
-	if (crank_coming)
-		crank_postponement++;
-	if (lane_change_postponement > LANE_CHANGE_POSTPONEMENT)
-	{
-		lane_change_coming = false;
-		y[MOTOR_LEFT] += 32;
-		// y[MOTOR_RIGHT] -= 30;
-		return;
-	}
-	if (crank_postponement > CRANK_POSTPONEMENT)
-	{
-		crank_coming = false;
-		y[MOTOR_LEFT] = -32;
-		y[MOTOR_RIGHT] = 30;
-		return;
-	}
-	if (left_line_crossed)
-	{
-		y[MOTOR_LEFT] -= 10;
-		y[MOTOR_RIGHT] += 10;
-		return;
-	}
-	if (right_line_crossed)
-	{
-		y[MOTOR_LEFT] += 10;
-		y[MOTOR_RIGHT] -= 10;
-		return;
-	}
-	y[MOTOR_LEFT] += 10;
-	y[MOTOR_RIGHT] += 10;
-}
-void MotorOut(const int8_t y[MOTOR_NUM])
-{
-	int8_t motor_left = MAP(y[MOTOR_LEFT]), motor_right = MAP(y[MOTOR_RIGHT]);
-	//-128 ~ -17 で後退,-16 ~ 15でブレーキ(0は空転), 16 ~ 127で前進
-	//ここ未完成なので回路と相談
-	for (size_t i = 0; i < MOTOR_RIGHT; i++)
-		MOTOR_WRITE_PWM(y[i], i);
-	return;
-}
-// void TMR0_Interrupt(void)
-// {
-// 	flag *= -1;
-// }
-void MOTOR_WRITE_PWM(const int8_t y[MOTOR_NUM], const uint8_t i, const uint8_t port)
-{
-	switch (port)
-	{
-	case 3:
-		PWM3_LoadDutyValue(y[i]);
-		break;
-	default:
-		break;
-	}
+  SYSTEM_Initialize();
+
+  int16_t motorOrder[MOTOR_NUM] = {64, 64};
+  bool left_line_crossed = PORTAbits.RA2, right_line_crossed = PORTAbits.RA3;
+
+  if (left_line_crossed)
+    motorOrder[MOTOR_LEFT] -= 32, motorOrder[MOTOR_RIGHT] += 32;
+  else if (right_line_crossed)
+    motorOrder[MOTOR_LEFT] += 32, motorOrder[MOTOR_RIGHT] -= 32;
+  else
+    motorOrder[MOTOR_LEFT] += 64, motorOrder[MOTOR_RIGHT] += 64;
+
+  int8_t motor[2] = {MAP(motorOrder[MOTOR_LEFT]), MAP(motorOrder[MOTOR_RIGHT])};
+
+  size_t j = 0;
+  while (HIGH)
+  {
+    if (16.0 < motor[MOTOR_RIGHT])
+    {
+      PORTCbits.RC4 = LOW;
+      PORTCbits.RC6 = HIGH;
+      PORTCbits.RC5 = HIGH;
+      if (j > UINT8_MAX / 2.0 * abs(motor[MOTOR_RIGHT]))
+        PORTCbits.RC5 = LOW;
+    }
+    else if (-16.0 > motor[MOTOR_RIGHT])
+    {
+      PORTCbits.RC4 = HIGH;
+      PORTCbits.RC6 = LOW;
+      PORTCbits.RC5 = HIGH;
+      if (j > UINT8_MAX / 2.0 * abs(motor[MOTOR_RIGHT]))
+        PORTCbits.RC5 = LOW;
+    }
+    if (16.0 < motor[MOTOR_LEFT])
+    {
+      PORTCbits.RC3 = HIGH;
+      PORTCbits.RC1 = LOW;
+      PORTCbits.RC2 = HIGH;
+      if (j > UINT8_MAX / 2.0 * abs(motor[MOTOR_LEFT]))
+        PORTCbits.RC2 = LOW;
+    }
+    else if (-16.0 > motor[MOTOR_LEFT])
+    {
+      PORTCbits.RC3 = LOW;
+      PORTCbits.RC1 = HIGH;
+      PORTCbits.RC2 = HIGH;
+      if (j > UINT8_MAX / 2.0 * abs(motor[MOTOR_LEFT]))
+        PORTCbits.RC2 = LOW;
+    }
+    j++;
+    __delay_us(1);
+  }
 }
--- linetrase.X/mcc_generated_files/mcc.h
+++ linetrase.X/mcc_generated_files/mcc.h
@@ -1,11 +1,15 @@
 /**
   @Generated PIC10 / PIC12 / PIC16 / PIC18 MCUs Header File
+
   @Company:
     Microchip Technology Inc.
+
   @File Name:
     mcc.h
+
   @Summary:
     This is the mcc.h file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
   @Description:
     This header file provides implementations for driver APIs for all modules selected in the GUI.
     Generation Information :
@@ -16,27 +20,32 @@
         Compiler          :  XC8 2.31 and above or later
         MPLAB             :  MPLAB X 5.45
 */
+
 /*
-    (c) 2018 Microchip Technology Inc. and its subsidiaries.
-    Subject to your compliance with these terms, you may use Microchip software and any
-    derivatives exclusively with Microchip products. It is your responsibility to comply with third party
-    license terms applicable to your use of third party software (including open source software) that
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
     may accompany Microchip software.
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY
-    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
     FOR A PARTICULAR PURPOSE.
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP
-    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO
-    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL
-    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT
-    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
     SOFTWARE.
 */
+
 #ifndef MCC_H
-#define MCC_H
+#define	MCC_H
 #include <xc.h>
 #include "device_config.h"
 #include "pin_manager.h"
@@ -47,7 +56,8 @@
 #include "pwm3.h"
 #include "tmr2.h"
 #include "pwm4.h"
-#include "adc.h"
+
+
 
 /**
  * @Param
@@ -61,6 +71,7 @@
     SYSTEM_Initialize(void);
  */
 void SYSTEM_Initialize(void);
+
 /**
  * @Param
     none
@@ -85,7 +96,8 @@ void OSCILLATOR_Initialize(void);
     WDT_Initialize(void);
  */
 void WDT_Initialize(void);
-#endif /* MCC_H */
+
+#endif	/* MCC_H */
 /**
  End of File
 */
