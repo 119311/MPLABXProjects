84da8dba1fd25fd33e61b76cd0c265ae354cff0c
adobesan <adobeoftheocean@outlook.jp>
2022-09-12 23:02:38 +0900

    format

--- analog.X/main.c
+++ analog.X/main.c
@@ -1,23 +1,21 @@
 #include "mcc_generated_files/mcc.h"
-#include <stdint.h>
 #include <stdbool.h>
-#include <stdlib.h>
 #include <stdint.h>
+#include <stdlib.h>
 #define _XTAL_FREQ 4000000
 #define PIN_INIT 0x00
 
 void main(void)
 {
-	SYSTEM_Initialize();
-	INTERRUPT_GlobalInterruptEnable();
-	INTERRUPT_PeripheralInterruptEnable();
-	LATB = PIN_INIT;
-	while (1)
-	{
-		if (!PORTAbits.RA0)
-			LATB = 0xff;
-		if (PORTAbits.RA0)
-			LATB = 0x00;
-		__delay_ms(10);
-	}
+    SYSTEM_Initialize();
+    INTERRUPT_GlobalInterruptEnable();
+    INTERRUPT_PeripheralInterruptEnable();
+    LATB = PIN_INIT;
+    while (1) {
+        if (!PORTAbits.RA0)
+            LATB = 0xff;
+        if (PORTAbits.RA0)
+            LATB = 0x00;
+        __delay_ms(10);
+    }
 }
