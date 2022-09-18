8289b24bf2d2df6e862ded393925ef39de98baf5
adobesan <adobeoftheocean@outlook.jp>
2022-09-19 01:04:33 +0900

    update motor

--- linetrase.X/main.c
+++ linetrase.X/main.c
@@ -3,11 +3,6 @@
 #include "motor.h"
 #include "pic16f1778.h"
 
-#define PORT1A IN2_LAT
-#define PORT1B IN1_LAT
-#define PORT2A IN3_LAT
-#define PORT2B IN4_LAT
-
 Motor left, right;
 LED led;
 
@@ -16,24 +11,27 @@ void run(void)
     runMotor(&left);
     runMotor(&right);
 }
-
-void main(void)
+void motorInit(void)
 {
-    SYSTEM_Initialize();
     setPort(&left, IN2_PORT, IN1_PORT);
     setPort(&right, IN3_PORT, IN4_PORT);
-    setSpeed(&left, 20);
-    setSpeed(&right, 20);
     setDirection(&left, true);
     setDirection(&right, true);
     MODE_LAT = LOW;
     LARGE_LAT = LOW;
+}
+void main(void)
+{
+    SYSTEM_Initialize();
+    motorInit();
     INTERRUPT_GlobalInterruptEnable();
     INTERRUPT_PeripheralInterruptEnable();
+    uint8_t leftSpeed = 20, rightSpeed = 20;
+    setSpeed(&left, leftSpeed);
+    setSpeed(&right, rightSpeed);
     while (true) {
         readLed(&led);
+        autoPilot(&led, &left, &right);
         asm("NOP");
-    
-    
     }
 }
--- linetrase.X/motor.c
+++ linetrase.X/motor.c
@@ -1,47 +1,36 @@
 #include "motor.h"
-
 int getSpeed(const Motor* self)
 {
     return self->speed;
 }
-
 int getDirection(const Motor* self)
 {
     return self->direction;
 }
-
 void setSpeed(Motor* self, uint8_t value)
 {
     self->speed = value;
 }
-
 void setDirection(Motor* self, bool value)
 {
     self->direction = value;
 }
-
 void setPort(Motor* self, bool port1, bool port2)
 {
-    self->port1 = &port1;
-    self->port2 = &port2;
+    self->port1 = &port1, self->port2 = &port2;
 }
-
 void runMotor(Motor* self)
 {
-    if (!self->tempTime) {
-        if (!self->speed) {
-            if (self->direction) {
-                *(self->port1) = HIGH;
-                *(self->port2) = LOW;
-            } else {
-                *(self->port1) = LOW;
-                *(self->port2) = HIGH;
-            }
-            self->speed--;
+    if (self->tempTime-- > 0) {
+        if (self->tempSpeed-- > 0) {
+            *(self->port1) = self->direction;
+            *(self->port2) = !self->direction;
+        } else {
+            *(self->port1) = LOW;
+            *(self->port2) = LOW;
         }
-        self->tempTime--;
-    } else {
-        self->tempSpeed = ((self->speed) > BASETIME) ? BASETIME : (self->speed);
+        } else {
+        self->tempSpeed = ((self->speed) > BASETIME) ? BASETIME : self->speed;
         self->tempTime = BASETIME;
     }
 }
