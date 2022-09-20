62c42573e2cb2ca34920544d21a23432d79f2a76
aaa <aaa>
2022-09-20 11:03:07 +0900

    update member

--- linetrase.X/src/LED.c
+++ linetrase.X/src/LED.c
@@ -2,12 +2,12 @@
 static uint16_t tempValue;
 void readLed(LED* self)
 {
-    self->LED1 = LED1_PORT;
-    self->LED2 = LED2_PORT;
-    self->LED4 = LED4_PORT;
-    self->LED5 = LED5_PORT;
-    self->LED7 = LED7_PORT;
-    self->LED8 = LED8_PORT;
-    self->LED3A = ADC_GetConversion(LED3A);
-    self->LED6A = ADC_GetConversion(LED6A);
+    self->_LED1 = LED1_PORT;
+    self->_LED2 = LED2_PORT;
+    self->_LED4 = LED4_PORT;
+    self->_LED5 = LED5_PORT;
+    self->_LED7 = LED7_PORT;
+    self->_LED8 = LED8_PORT;
+    self->_LED3A = ADC_GetConversion(LED3A);
+    self->_LED6A = ADC_GetConversion(LED6A);
 }
--- linetrase.X/src/LED.h
+++ linetrase.X/src/LED.h
@@ -1,16 +1,16 @@
 #pragma once
-#include "mcc_generated_files/mcc.h"
+#include "../mcc_generated_files/mcc.h"
 #include <stdbool.h>
 #include <stdint.h>
 typedef struct led_t LED;
 struct led_t {
-    bool LED1;
-    bool LED2;
-    uint16_t LED3A;
-    bool LED4;
-    bool LED5;
-    uint16_t LED6A;
-    bool LED7;
-    bool LED8;
+    bool _LED1;
+    bool _LED2;
+    uint8_t _LED3A;
+    bool _LED4;
+    bool _LED5;
+    uint8_t _LED6A;
+    bool _LED7;
+    bool _LED8;
 };
 void readLed(LED* self);
