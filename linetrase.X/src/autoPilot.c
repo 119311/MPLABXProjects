f7c9c92be5dd8e500af1d3c7cbbbd68b88a02417
aaa <aaa>
2022-09-27 10:50:24 +0900

    temp

--- linetrase.X/src/autoPilot.c
+++ linetrase.X/src/autoPilot.c
@@ -1,52 +1,52 @@
-#include "autoPilot.h"
-typedef enum {
-    LINETRACE,
-    CRANK,
-    CHICANE,
-} State;
-static bool hasCranked = false, hasChicaned = false;
-void autoPilot(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed)
-{
-    State state = getState(led);
-    switch (state) {
-    case LINETRACE:
-        lineTrace(led, leftSpeed, rightSpeed);
-        break;
-    case CRANK:
-        hasCranked = true;
-        crank(led, leftSpeed, rightSpeed);
-        break;
-    case CHICANE:
-        hasChicaned = true;
-        chicane(led, leftSpeed, rightSpeed);
-    }
-}
-State getState(LED* led)
-{
-    if (led->_LED1 && led->_LED2 && led->_LED7 && led->_LED8)
-        return CRANK;
-    if (led->_LED1 && led->_LED2)
-        return CHICANE;
-    return LINETRACE;
-}
-void lineTrace(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed)
-{
-    // pid
-    static float integral = 0.0;
-    static float derivative = 0.0;
-    static float lastError = 0.0;
-    float error = (float)(led->_LED3A - led->_LED6A);
-    integral += error * dt;
-    derivative = (error - lastError) / dt;
-    float pid = Kp * error + Ki * integral + Kd * derivative;
-    lastError = error;
-    *leftSpeed = (uint8_t)(*leftSpeed + pid);
-    *rightSpeed = (uint8_t)(*rightSpeed - pid);
-    return;
-}
-void crank(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed)
-{
-}
-void chicane(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed)
-{
-}
+//#include "autoPilot.h"
+//typedef enum {
+//    LINETRACE,
+//    CRANK,
+//    CHICANE,
+//} State;
+//static bool hasCranked = false, hasChicaned = false;
+//void autoPilot(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed)
+//{
+//    State state = getState(led);
+//    switch (state) {
+//    case LINETRACE:
+//        lineTrace(led, leftSpeed, rightSpeed);
+//        break;
+//    case CRANK:
+//        hasCranked = true;
+//        crank(led, leftSpeed, rightSpeed);
+//        break;
+//    case CHICANE:
+//        hasChicaned = true;
+//        chicane(led, leftSpeed, rightSpeed);
+//    }
+//}
+//State getState(LED* led)
+//{
+//    if (led->_LED1 && led->_LED2 && led->_LED7 && led->_LED8)
+//        return CRANK;
+//    if (led->_LED1 && led->_LED2)
+//        return CHICANE;
+//    return LINETRACE;
+//}
+//void lineTrace(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed)
+//{
+//    // pid
+//    static float integral = 0.0;
+//    static float derivative = 0.0;
+//    static float lastError = 0.0;
+//    float error = (float)(led->_LED3A - led->_LED6A);
+//    integral += error * dt;
+//    derivative = (error - lastError) / dt;
+//    float pid = Kp * error + Ki * integral + Kd * derivative;
+//    lastError = error;
+//    *leftSpeed = (uint8_t)(*leftSpeed + pid);
+//    *rightSpeed = (uint8_t)(*rightSpeed - pid);
+//    return;
+//}
+//void crank(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed)
+//{
+//}
+//void chicane(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed)
+//{
+//}
--- linetrase.X/src/autoPilot.h
+++ linetrase.X/src/autoPilot.h
@@ -1,9 +1,9 @@
-#include "lineTracer.h"
-#include <stdbool.h>
-#include <stdint.h>
-typedef enum State;
-void autoPilot(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed);
-State getState(LED* led);
-void lineTrace(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed);
-void crank(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed);
-void chicane(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed);
+//#include "lineTracer.h"
+//#include <stdbool.h>
+//#include <stdint.h>
+//typedef enum State;
+//void autoPilot(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed);
+//State getState(LED* led);
+//void lineTrace(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed);
+//void crank(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed);
+//void chicane(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed);
