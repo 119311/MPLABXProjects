47500039642c68b8d8f5778a15f061df3926d3b9
aaa <aaa>
2022-10-21 19:25:41 +0900

    temp

--- linetrase.X/src/autoPilot.h
+++ linetrase.X/src/autoPilot.h
@@ -1,9 +1,19 @@
-//#include "lineTracer.h"
-//#include <stdbool.h>
-//#include <stdint.h>
-//typedef enum State;
-//void autoPilot(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed);
-//State getState(LED* led);
-//void lineTrace(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed);
-//void crank(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed);
-//void chicane(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed);
+#ifndef AUTOPILOT_H
+#define AUTOPILOT_H
+
+#include "lineTracer.h"
+#define Kp 0.05
+
+typedef enum {
+    LINETRACE,
+    CRANK,
+    CHICANE,
+} State;
+
+void autoPilot(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed);
+// State getState(LED* led);
+// void lineTrace(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed);
+// void crank(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed);
+// void chicane(LED* led, uint8_t* leftSpeed, uint8_t* rightSpeed);
+
+#endif
